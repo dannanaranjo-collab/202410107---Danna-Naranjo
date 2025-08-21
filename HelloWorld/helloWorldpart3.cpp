64f4db021ef103c7b7249b15dd66ed8ebe79484f
dannanaranjo-collab <danna.naranjo@uptc.edu.co>
2025-08-21 16:50:09 +0000

    Mi nombre y mi edad

---
+++ HelloWorld/helloWorldpart3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+    string userName ="Sofia";
+    int userAge = 18;
+    cout << "Hello" << " - " << userName << endl;
+    cout << "Your age is " << userAge << endl; 
+    cout << endl;
+    return 0;
+}
